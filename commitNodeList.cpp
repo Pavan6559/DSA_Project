186c6aeb104e54565b51d69eb7dd0e9790e85994
siddhardha-39 <saisiddhardak@gmail.com>
2025-11-07 23:21:25 +0530

    Update commitNodeList.cpp

--- commitNodeList.cpp
+++ commitNodeList.cpp
@@ -13,3 +13,24 @@ using namespace std;
 #else
     #include <unistd.h>
 #endif
+
+// ======================================================
+//  GLOBAL HELPER FUNCTIONS (used for commit ID + timestamp)
+// ======================================================
+
+string gen_random(const int len)
+{
+    srand((unsigned)time(NULL) * getpid());
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    string tmp;
+    tmp.reserve(len);
+
+    for (int i = 0; i < len; ++i)
+        tmp += alphanum[rand() % (sizeof(alphanum) - 1)];
+
+    return tmp;
+}
