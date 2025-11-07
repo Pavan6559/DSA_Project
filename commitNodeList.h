52889bd0e79a11139560eb5f9ffc2fa5004ea9a3
ashishb24cs1024 <b24cs1024@iitj.ac.in>
2025-11-07 23:14:26 +0530

    Update commitNodeList.h

--- commitNodeList.h
+++ commitNodeList.h
@@ -1,2 +1,10 @@
 #pragma once
 #include <string>
+class commitNodeList
+{
+public:
+    commitNodeList();
+    void addOnTail(std::string msg);
+    void revertCommit(std::string commitHash);
+    void printCommitList();
+};
