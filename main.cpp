724abd40b96e19cb0f26a36043173bb7e4ba340f
ashishb24cs1024 <b24cs1024@iitj.ac.in>
2025-11-10 23:02:52 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,5 +15,116 @@ using namespace std;
 //---------------------
 
 int main(){
-  
+  gitClass gitClassObj;
+    if(argc >= 2)
+    {
+        string argument = string(argv[1]);
+        //git init
+        if (argument == "init")
+        {
+            gitClassObj.gitInit();
+            cout <<GRN "git repository initialized successfully!" END<< endl;
+            
+        }
+        //git add
+        else if (argument == "add")                                               
+        {
+            if(argc == 2){     
+                cout << RED "missing arguments!" <<endl;
+                cout << "Provide a third argument e.g." << endl;
+                cout << "git add <'.' | 'file_name'>" END << endl;
+            }
+            if(argc >= 3){
+                if(argc == 3){
+                    string argumentC = string(argv[2]);
+                    if (argumentC == ".") {
+                        gitClassObj.gitAdd();
+                    }
+                    else
+                    {
+                        string files[1] = {string(argv[2])};
+                        gitClassObj.gitAdd(files, 1);
+                    }
+
+                } else {
+                    vector<string> files;
+                    for (int i = 2; i < argc; i++)
+                    {
+                        files.push_back(string(argv[i]));
+                    }
+                    gitClassObj.gitAdd(files.data(), files.size());
+                }
+            }
+            
+        }
+        //git commit
+        else if (argument == "commit")
+        {
+            if(argc == 4)    //[ git, commit, -m, "msg" ]
+            {
+                string argumentC = string(argv[2]);    
+                string argumentD = string(argv[3]);      
+                if(argumentC == "-m")
+                {
+                    gitClassObj.gitCommit(argumentD);
+                    cout << "files commited successfully" << endl;
+                }
+            }
+            else 
+            {
+                cout << RED "missing arguments!" <<endl;
+                cout << "Provide with a message field e.g." << endl;
+                cout << "git commit -m 'my commit message'" END << endl;
+            }
+        }
+        // git revert
+        else if(argument == "revert")
+        {
+            if(argc == 3)
+            {
+                string argumentC = string(argv[2]);
+                if(argumentC == "HEAD")
+                {
+                    gitClassObj.gitRevert(argumentC);
+                    cout << "The project is now at HEAD" << endl;
+                }
+                else
+                {
+                    gitClassObj.gitRevert(argumentC);
+                    cout << "Reverted to <commit_id> commit" << endl;
+                }
+            }
+            else 
+            {
+                cout << RED "invalid arguments, should be like: " << endl;
+                cout << "git revert <'HEAD'|'commit_hash'>" END<< endl;
+            }
+        }
+        // //git log
+        else if(argument == "log")
+        {
+            gitClassObj.gitLog();
+        }
+        //git status
+        // else if(argument == "status")
+        // {
+        //     gitClassObj.gitStatus();
+        // }
+        //wrong arguments
+        else
+        {
+            cout << RED "Invalid arguments" END << endl;
+        }
+
+    }
+    else 
+    {
+        cout << YEL "git is version control system made by linus Torvalds, this project is a clone of that original system with minimal features \n\n";
+
+        cout << "Usage: " << endl;
+        cout << "git init                           ->   initialize an empty git repository in the current dir" << endl;
+        cout << "git add <'.'|'file_name'>          ->   add the files to staging area" << endl;
+        cout << "git commit <m 'commit message'>    ->   commit your staging files" << endl;
+        cout << "git revert <'HEAD'|'commit_hash'>  ->   rollback to a specific commit" END << endl;
+    }
 }
